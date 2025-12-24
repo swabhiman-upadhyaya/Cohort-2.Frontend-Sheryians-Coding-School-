e3ca202e15b4b80f189ec6ee5d4225b254cdc5ae
Swabhiman Upadhyaya <swabhimanupadhyaya18@gmail.com>
2025-12-24 22:23:08 +0530

    27.task

--- 26.task React/src/components/Footer.jsx
+++ 26.task React/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer>
         <button className="fstBtn">You need a designer</button>
-        <img src="./src/img1.jpg" alt=""/>
+        <img src="./src/assets/img1.png" alt=""/>
         <button className="secBtn">You need a Photographer</button>
     </footer>
   )
