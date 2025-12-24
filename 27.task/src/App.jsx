c4389063b4bb84a73afcbca0651d9a4e48a49ba0
Swabhiman Upadhyaya <swabhimanupadhyaya18@gmail.com>
2025-12-24 22:35:05 +0530

    27.task

---
+++ 27.task/src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from './components/Nav.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  )
+}
+
+export default App
---
+++ 27.task/src/components/Nav.jsx
@@ -0,0 +1,18 @@
+const Nav = () => {
+    return (
+        <nav className="sticky top-0 flex font-[Helvetica] h-16 bg-black text-white p-5 justify-between items-center">
+            <div>
+                <p className="text-2xl font-extralight">DVSY</p>
+            </div>
+            <div className="flex gap-5 items-center">
+                <a href="#" className="px-2.5 py-1 bg-stone-700 text-1xl rounded-sm text-sm font-normal%">DESIGNERS</a>
+                <a href="#" className="px-2.5 py-1 bg-stone-700 text-1xl rounded-sm text-sm font-normal">COLLABS</a>
+                <a href="#" className="px-2.5 py-1 bg-stone-700 text-1xl rounded-sm text-sm font-normal">EVENTS</a>
+                <a href="#" className="px-2.5 py-1 bg-stone-700 text-1xl rounded-sm text-sm font-normal">BLOG</a>
+                <a href="#" className="px-2.5 py-1 bg-stone-700 text-1xl rounded-sm text-sm font-normal">CARD</a>
+                <button className="px-3 py-2 bg-orange-700 text-black rounded-sm font-semibold text-sm">GET IN TOUCH</button>
+            </div>
+        </nav>
+    )
+}
+export default Nav
