10c11d801ef830351e3351114a832f2909ac31fe
Swabhiman Upadhyaya <swabhimanupadhyaya18@gmail.com>
2025-12-25 23:50:29 +0530

    28.task Completed

--- 28.task/src/App.jsx
+++ 28.task/src/App.jsx
@@ -2,8 +2,27 @@ import Card from './components/Card.jsx'
 
 const App = () => {
   return (
-    <div>
-        <Card />
+    <div className='p-3 flex gap-6 flex-wrap justify-center bg-gray-600 text-white'>
+      <Card backImg="./src/assets/img1.jpg" profilePic="./src/assets/img2.jpg" name="Aarohi Mehta" designation="UI Designer" likeCnt="42.3k" postCnt="310" views="390k" />
+
+      <Card backImg="./src/assets/img2.jpg" profilePic="./src/assets/img1.jpg" name="Neha Kapoor" designation="Frontend Developer" likeCnt="61.8k" postCnt="520" views="610k" position="top" />
+
+      <Card backImg="./src/assets/img3.jpg" profilePic="./src/assets/img3.jpg" name="Riya Verma" designation="Product Manager" likeCnt="37.6k" postCnt="280" views="350k" />
+
+      <Card backImg="./src/assets/img4.jpg" profilePic="./src/assets/img4.jpg" name="Simran Kaur" designation="Digital Marketer" likeCnt="74.2k" postCnt="640" views="720k" />
+
+      <Card backImg="./src/assets/img2.jpg" profilePic="./src/assets/img2.jpg" name="Ananya Singh" designation="Backend Developer" likeCnt="29.4k" postCnt="190" views="260k" position="top" />
+
+      <Card backImg="./src/assets/img1.jpg" profilePic="./src/assets/img3.jpg" name="Pooja Nair" designation="UX Researcher" likeCnt="53.9k" postCnt="410" views="500k" />
+
+      <Card backImg="./src/assets/img3.jpg" profilePic="./src/assets/img1.jpg" name="Kritika Joshi" designation="Tech Blogger" likeCnt="88.1k" postCnt="710" views="910k" />
+
+      <Card backImg="./src/assets/img2.jpg" profilePic="./src/assets/img4.jpg" name="Isha Malhotra" designation="Brand Strategist" likeCnt="46.7k" postCnt="360" views="430k" position="top" />
+
+      <Card backImg="./src/assets/img4.jpg" profilePic="./src/assets/img2.jpg" name="Shreya Choudhary" designation="Data Analyst" likeCnt="34.8k" postCnt="250" views="300k" />
+
+      <Card backImg="./src/assets/img1.jpg" profilePic="./src/assets/img1.jpg" name="Anika Bose" designation="Content Creator" likeCnt="92.5k" postCnt="820" views="1.1M" />
+
     </div>
   )
 }
--- 28.task/src/components/Card.jsx
+++ 28.task/src/components/Card.jsx
@@ -1,75 +1,43 @@
-const Card = () => {
+const Card = (props) => {
+
+  const stats = [
+    { value: props.likeCnt, label: "Likes" },
+    { value: props.postCnt, label: "Post" },
+    { value: props.views, label: "Views" },
+  ];
+
   return (
-    <div className="p-3 flex gap-6 flex-wrap justify-center">
-      <div className="card relative h-90 p-1 w-65 bg-gray-400 rounded-md">
-        
+    <div>
+
+      <div className="card relative p-1 w-65 bg-black rounded-md">
+
         <div className="imgs p-1 relative">
-          <img src="./src/assets/img.png" alt="" className="w-65 object-cover rounded-sm"/>
-          <img src="./src/assets/img.png" alt="" className="absolute left-[5rem] bottom-[-30px] rounded-full size-20"/>
-        </div>
-        
-        <div className="profile flex flex-col items-center mt-7">
-          <p className="text-[1.6rem] font-normal">Priya Sharma</p>
-          <p className="text-lg font-">Content Creator</p>
-        </div>
-        
-        <div className="counts flex gap-10 justify-center font-medium text-[1.1rem] mt-4">
-          <div className="likes text-center">
-            <p className="text-sm">55.1k</p>
-            <p className="text-[1.3rem]">Likes</p>
-          </div>
-          <div className="posts text-center">
-            <p className="text-sm">400</p>
-            <p className="text-[1.3rem]">Post</p>
-          </div>
-          <div className="views text-center">
-            <p className="text-sm">480k</p>
-            <p className="text-[1.3rem]">views</p>
-          </div>
+          <img src={props.backImg} alt="" className="w-65 h-40 object-cover rounded-sm" />
+          <img src={props.profilePic} alt="" className="absolute left-[5rem] bottom-[-30px] rounded-full size-20 object-cover" />
         </div>
 
-        <div className="icons text-3xl flex gap-7 mt-4 justify-center">
+        <div className="profile flex flex-col items-center mt-6.5">
+          <p className="text-[1.4rem] font-bold">{props.name}</p>
+          <p className="text-[1rem] text-gray-500 font-bold">{props.designation}</p>
+        </div>
+
+        <div className="counts flex gap-8 justify-center font-medium text-[1.1rem]">
+          {stats.map((item, i) => (
+            <div key={i} className="text-center">
+              <p className="text-sm font-black">{item.value}</p>
+              <p className="text-[1.2rem]">{item.label}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="icons text-3xl flex gap-5 justify-center">
           <i class="ri-linkedin-box-fill text-blue-700"></i>
-          <i class="ri-github-fill text-black"></i>
+          <i class="ri-github-fill"></i>
           <i class="ri-instagram-line text-pink-700"></i>
         </div>
 
       </div>
 
-      <div className="card relative h-90 p-1 w-65 bg-gray-400 rounded-md">
-        
-        <div className="imgs p-1 relative">
-          <img src="./src/assets/img.png" alt="" className="w-65 object-cover rounded-sm"/>
-          <img src="./src/assets/img.png" alt="" className="absolute left-[5rem] bottom-[-30px] rounded-full size-20"/>
-        </div>
-        
-        <div className="profile flex flex-col items-center mt-7">
-          <p className="text-[1.6rem] font-normal">Priya Sharma</p>
-          <p className="text-lg font-">Content Creator</p>
-        </div>
-        
-        <div className="counts flex gap-10 justify-center font-medium text-[1.1rem] mt-4">
-          <div className="likes text-center">
-            <p className="text-sm">55.1k</p>
-            <p className="text-[1.3rem]">Likes</p>
-          </div>
-          <div className="posts text-center">
-            <p className="text-sm">400</p>
-            <p className="text-[1.3rem]">Post</p>
-          </div>
-          <div className="views text-center">
-            <p className="text-sm">480k</p>
-            <p className="text-[1.3rem]">views</p>
-          </div>
-        </div>
-
-        <div className="icons text-3xl flex gap-7 mt-4 justify-center">
-          <i class="ri-linkedin-box-fill text-blue-700"></i>
-          <i class="ri-github-fill text-black"></i>
-          <i class="ri-instagram-line text-pink-700"></i>
-        </div>
-        
-      </div>
     </div>
   )
 }
